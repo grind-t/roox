b95a4694fdc6841bea0ee9a288ed94fcafaed791
grind-t <grindt.gd@gmail.com>
2022-03-13 19:51:42 +0300

    feat: routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./app.module.scss";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return <div className={styles.hello}>hello</div>;
+  return (
+    <div className={styles.hello}>
+      <Outlet />
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserList from "./components/user-list";
+import UserProfile from "./components/user-profile";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<UserList />} />
+          <Route path="profile" element={<UserProfile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
