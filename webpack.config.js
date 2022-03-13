4e82f3864c91bfd9e6c77b13d3b67c03ade749de
grind-t <grindt.gd@gmail.com>
2022-03-13 15:52:27 +0300

    build: configure webpack development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "RooX",
