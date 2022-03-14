b01a43cd551088337cd51b03977583836886c8cc
grind-t <grindt.gd@gmail.com>
2022-03-14 14:10:16 +0300

    refactor: users lib

--- src/components/users/user-list/user-list.tsx
+++ src/components/users/user-list/user-list.tsx
@@ -2,9 +2,8 @@ import React, { useMemo } from "react";
 import styles from "./user-list.module.scss";
 import UserListItem from "./user-list-item";
 import Loader from "../../common/loader";
-import { compareStrings } from "../../../utils/strings";
 import { useSortBy, useUsers } from "../../../hooks/users";
-import { SortBy } from "../../../lib/users";
+import { sortUsers } from "../../../lib/users";
 
 export default function UserList() {
   const { data: users, error } = useUsers();
@@ -12,13 +11,7 @@ export default function UserList() {
 
   const sortedUsers = useMemo(() => {
     if (!users) return;
-    let compareFn: (a: any, b: any) => number;
-    if (sortBy === SortBy.City)
-      compareFn = (a, b) => compareStrings(a.address.city, b.address.city);
-    else if (sortBy === SortBy.Company)
-      compareFn = (a, b) => compareStrings(a.company.name, b.company.name);
-    else compareFn = (a, b) => compareStrings(a.name, b.name);
-    return users.sort(compareFn);
+    return sortUsers(users, sortBy);
   }, [users, sortBy]);
 
   if (error) return <div>Ошибка 😢</div>;
--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,3 +1,21 @@
+import { compareStrings } from "../utils/strings";
+
+export interface User {
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+    zipcode: string;
+  };
+  company: {
+    name: string;
+  };
+  phone: string;
+  website: string;
+}
+
 export enum SortBy {
   Name,
   City,
@@ -6,6 +24,25 @@ export enum SortBy {
 
 const apiUrl = "https://jsonplaceholder.typicode.com";
 
-export function getUsers(): Promise<any[]> {
+export function getUsers(): Promise<User[]> {
   return fetch(`${apiUrl}/users`).then((res) => res.json());
+}
+
+export function getSortingProperty(user: User, sortBy: SortBy): string {
+  switch (sortBy) {
+    case SortBy.City:
+      return user.address.city;
+    case SortBy.Company:
+      return user.company.name;
+    default:
+      return user.name;
+  }
+}
+
+export function sortUsers(users: User[], by: SortBy) {
+  return users.sort((a, b) => {
+    const p1 = getSortingProperty(a, by);
+    const p2 = getSortingProperty(b, by);
+    return compareStrings(p1, p2);
+  });
 }
